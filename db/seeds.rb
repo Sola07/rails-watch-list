4d3b47bb517c63fcfa9d798931c915571cf373e9
Flavie Guiselin <flavie.sola.guiselin@gmail.com>
2024-03-08 15:49:58 +0100

    some front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,56 +2,19 @@ require "json"
 require "open-uri"
 key = 'api_key=f9fdebbc8388d6939e502387da8c1e28'
 
-url = 'https://tmdb.lewagon.com/movie/top_rated'
-movies_serialized = URI.open(url).read
-movies = JSON.parse(movies_serialized)["results"]
-
-movies.each do |movie|
-  # puts "#{movie["original_title"]}, #{movie["overview"]}, #{movie['poster_path']}, #{movie["vote_average"]}"
-  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",rating: movie["vote_average"].to_i)
+url = "https://api.themoviedb.org/3/movie/top_rated?#{key}&"
+print = 20.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(URI.open("#{url}page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/w500/"
+    Movie.create(
+      title: movie['original_title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['poster_path']}",
+      rating: movie['vote_average'].to_i
+    )
+  end
 end
-
-url2 = "https://api.themoviedb.org/3/movie/top_rated?#{key}&page=2"
-movies_serialized2 = URI.open(url2).read
-movies2 = JSON.parse(movies_serialized2)["results"]
-
-movies2.each do |movie|
-  # puts "#{movie["original_title"]}, #{movie["overview"]}, #{movie['poster_path']}, #{movie["vote_average"]}"
-  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",rating: movie["vote_average"].to_i)
-end
-
-url3 = "https://api.themoviedb.org/3/movie/top_rated?#{key}&page=3"
-movies_serialized3 = URI.open(url3).read
-movies3 = JSON.parse(movies_serialized3)["results"]
-
-movies3.each do |movie|
-  # puts "#{movie["original_title"]}, #{movie["overview"]}, #{movie['poster_path']}, #{movie["vote_average"]}"
-  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",rating: movie["vote_average"].to_i)
-end
-
-url4 = "https://api.themoviedb.org/3/movie/top_rated?#{key}&page=4"
-movies_serialized4 = URI.open(url4).read
-movies4 = JSON.parse(movies_serialized4)["results"]
-
-movies4.each do |movie|
-  # puts "#{movie["original_title"]}, #{movie["overview"]}, #{movie['poster_path']}, #{movie["vote_average"]}"
-  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",rating: movie["vote_average"].to_i)
-end
-
-url5 = "https://api.themoviedb.org/3/movie/top_rated?#{key}&page=5"
-movies_serialized5 = URI.open(url5).read
-movies5 = JSON.parse(movies_serialized5)["results"]
-
-movies5.each do |movie|
-  # puts "#{movie["original_title"]}, #{movie["overview"]}, #{movie['poster_path']}, #{movie["vote_average"]}"
-  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",rating: movie["vote_average"].to_i)
-end
-
-url6 = "https://api.themoviedb.org/3/movie/top_rated?#{key}&page=6"
-movies_serialized6 = URI.open(url6).read
-movies6 = JSON.parse(movies_serialized6)["results"]
-
-movies6.each do |movie|
-  # puts "#{movie["original_title"]}, #{movie["overview"]}, #{movie['poster_path']}, #{movie["vote_average"]}"
-  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",rating: movie["vote_average"].to_i)
-end
+puts print
